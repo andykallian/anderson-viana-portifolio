916bdd3facda80cd5ab7ebe2dcdb9727c2482d6c
AndyKallian <andersonkallian@yahoo.com.br>
2023-07-27 19:27:59 -0300

    first commit

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,42 @@
+import github from '../assets/github.svg'
+
+const SocialMediaIcons = () => {
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.linkedin.com/vianna-anderson"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="linkedin-link" src="../assets/linkedin.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://github.com/andykallian"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="twitter-link" src={github} />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.facebook.com/ViannaAndy"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="facebook-link" src="../assets/facebook.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.instagram.com/andyvianna_/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="instagram-link" src="../assets/instagram.png" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
